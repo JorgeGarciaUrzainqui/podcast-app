de1258cc4626d3503d87cf328e3d517487835009
Jorge Garcia Urzainqui <jorge-alexander.garcia-urzainqui@capgemini.com>
2023-04-11 11:38:28 +0200

    Fix JSDoc for ClickableText component

--- src/components/ui/molecules/ClickableText/ClickableText.jsx
+++ src/components/ui/molecules/ClickableText/ClickableText.jsx
@@ -7,10 +7,8 @@ import Text from '../../atoms/Text';
  * Generates a clickable container with a text component
  *
  * @param {Object} props - props of the component
- * @param {string} props.inputValue - the current value of the input
- * @param {Function} [props.onInputChange] - Optional. The function that handles the input change
- * @param {string} [props.inputPlaceholder] - Optional. The placeholder of the input
- * @param {boolean} [props.isReadOnly] - Optional. Indicates fi the input is read only
+ * @param {string} props.text - the text to render
+ * @param {Function} props.onClickTex - The function that handles the click
  * @param {string} [props.variantClass] - Optional. Allows to inject css classes to the component.
  * @param {string} [props.testId] - Optional. For testing purposes only. Indicates the data-testid to be used.
  * @returns JSX Element - the text input component
