1dfaf8e051fab40f69dda38d1a37536fd29f1cd2
Jorge Garcia Urzainqui <jorge-alexander.garcia-urzainqui@capgemini.com>
2023-04-13 08:04:15 +0200

    Solve test problem with mock table header

--- tests/components/ui/atoms/Table/__mock__/MockTableChildren.jsx
+++ tests/components/ui/atoms/Table/__mock__/MockTableChildren.jsx
@@ -1,10 +1,12 @@
 const MockTableChildren = ({ testId }) => {
   return (
-    <tr data-testid={testId}>
-      <td>Property 1</td>
-      <td>Property 1</td>
-      <td>Property 1</td>
-    </tr>
+    <thead>
+      <tr data-testid={testId}>
+        <td>Property 1</td>
+        <td>Property 1</td>
+        <td>Property 1</td>
+      </tr>
+    </thead>
   );
 };
 
